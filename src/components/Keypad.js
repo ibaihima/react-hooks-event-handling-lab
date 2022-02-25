95997a252a9492dd32b1091c24aae35bc80bec18
Ibaihima <ibaihima@hotmail.com>
2022-02-25 12:05:46 -0500

    commit tree 5ebe0285827cefdb08c076fd45170a24d48cc541

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad () {
+    const text = () =>{
+        console.log ('Entering password...')
+    }
+    return (
+        <input type ="password" onChange={text}/>
+    )
+}
+
+
+
+export default Keypad;
